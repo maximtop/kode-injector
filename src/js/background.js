23c87010a684f8f0ff09d8c2b39909573981de21
Topciu Maxim <maximtop@gmail.com>
2018-01-09 11:59:04 +0300

    add style injection to the page

--- src/js/background.js
+++ src/js/background.js
@@ -2,7 +2,12 @@ import url from 'url';
 import '../img/icon-128.png';
 import '../img/icon-34.png';
 
-const bindings = { 'stackoverflow.com': { js: 'file:///home/maxim/Documents/test.js' } };
+const bindings = {
+  'stackoverflow.com': {
+    jsFilePath: 'file:///home/maxim/Documents/test.js',
+    cssFilePath: 'file:///home/maxim/Documents/test.css',
+  },
+};
 
 const readFile = filePath => new Promise(((resolve, reject) => {
   const xhr = new XMLHttpRequest();
@@ -21,18 +26,21 @@ const readFile = filePath => new Promise(((resolve, reject) => {
 chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
   if (changeInfo.status === 'loading') {
     const tabUrl = url.parse(tab.url);
-    const jsFilePath = bindings[tabUrl.hostname].js;
-    let code;
-    try {
-      code = await readFile(jsFilePath);
-    } catch (e) {
-      console.log('Error: ', e);
+    const { jsFilePath, cssFilePath } = bindings[tabUrl.hostname];
+    let jsCode;
+    let cssCode;
+    if (jsFilePath) {
+      jsCode = await readFile(jsFilePath);
     }
-    if (code) {
-      // save code to chrome.storage.local
-      chrome.storage.local.set({ kInjector: { [tabUrl.hostname]: { code } } }, (e) => {
+    if (cssFilePath) {
+      cssCode = await readFile(cssFilePath);
+    }
+    // save code to chrome.storage.local
+    chrome.storage.local.set(
+      { kInjector: { [tabUrl.hostname]: { jsCode, cssCode } } },
+      (e) => {
         console.log('successfully saved', e);
-      });
-    }
+      },
+    );
   }
 });
--- src/js/content.js
+++ src/js/content.js
@@ -4,13 +4,25 @@ const head = document.getElementsByTagName('head')[0];
 const script = document.createElement('script');
 script.setAttribute('data-source', 'Kameleoon injector');
 script.type = 'text/javascript';
+
+const style = document.createElement('style');
+style.setAttribute('data-source', 'Kameleoon Injector');
+style.type = 'text/css';
 const tabUrl = url.parse(window.location.href);
 const tabHostname = tabUrl.hostname;
 chrome.storage.local.get('kInjector', ({ kInjector }) => {
-  const { code } = kInjector[tabHostname];
-  if (code) {
-    script.text = code;
+  const { jsCode, cssCode } = kInjector[tabHostname];
+  if (jsCode) {
+    script.text = jsCode;
     head.appendChild(script);
+  }
+  if (cssCode) {
+    if (style.styleSheet) { // IE Explorer
+      style.styleSheet.cssText = cssCode;
+    } else {
+      style.appendChild(document.createTextNode(cssCode));
+    }
+    head.appendChild(style);
   }
 });
 
