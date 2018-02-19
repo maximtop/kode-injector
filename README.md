1cb301f591f74f423b53ec0d77db89667a8dbb13
Maxim Topchu <maximtop@gmail.com>
2018-02-19 21:35:42 +0300

    change run at time for content script

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Kameleoon Injector
 
 Chrome extension for JS and CSS files injection to the specified websites
+
+## How to install for developing
+
+* `git clone`
+* `yarn install`
+* `make build`
+* `Turn on developer mode in your chrome`
+* `install unpaked extension from the build folder`
--- src/js/content.js
+++ src/js/content.js
@@ -1,44 +1,54 @@
 import url from 'url';
 import { getState } from './helpers/chromeStorage';
 
-const dataSource = 'Kameleoon Injector';
-const head = document.getElementsByTagName('head')[0];
-const script = document.createElement('script');
-script.setAttribute('data-source', dataSource);
-script.type = 'text/javascript';
 
-const style = document.createElement('style');
-style.setAttribute('data-source', dataSource);
-style.type = 'text/css';
-const tabUrl = url.parse(window.location.href);
-const tabHostname = tabUrl.hostname;
-chrome.storage.local.get('config', async ({ config }) => {
-// eslint-disable-next-line no-prototype-builtins
-  const { isActivated } = await getState('isActivated');
-  if (config.hasOwnProperty(tabHostname) && isActivated) {
-    const { jsCode, cssCode } = config[tabHostname];
-    if (cssCode) {
-      if (cssCode.error) {
-        console.log(cssCode.error);
-      } else {
-        if (style.styleSheet) { // IE Explorer
-          style.styleSheet.cssText = cssCode;
+const start = () => {
+  const dataSource = 'Kameleoon Injector';
+  const head = document.getElementsByTagName('head')[0];
+  const script = document.createElement('script');
+  script.setAttribute('data-source', dataSource);
+  script.type = 'text/javascript';
+
+  const style = document.createElement('style');
+  style.setAttribute('data-source', dataSource);
+  style.type = 'text/css';
+  const tabUrl = url.parse(window.location.href);
+  const tabHostname = tabUrl.hostname;
+  chrome.storage.local.get('config', async ({ config }) => {
+    // eslint-disable-next-line no-prototype-builtins
+    const { isActivated } = await getState('isActivated');
+    if (config.hasOwnProperty(tabHostname) && isActivated) {
+      const { jsCode, cssCode } = config[tabHostname];
+      if (cssCode) {
+        if (cssCode.error) {
+          console.log(cssCode.error);
         } else {
-          style.appendChild(document.createTextNode(cssCode));
+          if (style.styleSheet) { // IE Explorer
+            style.styleSheet.cssText = cssCode;
+          } else {
+            style.appendChild(document.createTextNode(cssCode));
+          }
+          head.appendChild(style);
+          console.log('content script css', Date.now());
         }
-        head.appendChild(style);
-        console.log('content script css', Date.now());
+      }
+      if (jsCode) {
+        if (jsCode.error) {
+          console.log(jsCode.error);
+        } else {
+          script.text = jsCode;
+          head.appendChild(script);
+          console.log('content script js', Date.now());
+        }
       }
     }
-    if (jsCode) {
-      if (jsCode.error) {
-        console.log(jsCode.error);
-      } else {
-        script.text = jsCode;
-        head.appendChild(script);
-        console.log('content script js', Date.now());
-      }
-    }
+  });
+};
+
+const check = setInterval(() => {
+  if (document.getElementsByTagName('head')) {
+    clearInterval(check);
+    start();
   }
 });
 
